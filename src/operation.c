a8499e4c40575aa2cbb87aebde202f5a57c82ecf
Michal Mrnustik <michal.mrnustik@outlook.com>
2017-02-23 17:41:10 +0100

    Fix bad usage of strcmp

--- src/operation.c
+++ src/operation.c
@@ -6,27 +6,27 @@
 
 OperationType parseOperation(char* opString)
 {
-    if(strcmp(opString, "del"))
+    if(strcmp(opString, "del") == 0)
     {
         return DELETE;
     }
-    else if(strcmp(opString, "get"))
+    else if(strcmp(opString, "get") == 0)
     {
         return DOWNLOAD;
     }
-    else if(strcmp(opString, "put"))
+    else if(strcmp(opString, "put") == 0)
     {
         return UPLOAD;
     }
-    else if(strcmp(opString, "lst"))
+    else if(strcmp(opString, "lst") == 0)
     {
         return LIST;
     }
-    else if(strcmp(opString, "mkd"))
+    else if(strcmp(opString, "mkd") == 0)
     {
         return MAKE_DIR;
     }
-    else if(strcmp(opString, "rmd"))
+    else if(strcmp(opString, "rmd") == 0)
     {
         return REMOVE_DIR;
     }

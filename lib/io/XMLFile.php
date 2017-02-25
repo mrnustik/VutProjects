2cd61043cad101dcc2ab407b0cd1fc92efa6fa92
Michal Mrnustik <michal.mrnustik@outlook.com>
2017-02-25 21:55:06 +0100

    Add comments for XMLFile

--- lib/io/XMLFile.php
+++ lib/io/XMLFile.php
@@ -1,15 +1,18 @@
 <?php
 
 /**
- * Created by PhpStorm.
- * User: mrnda
- * Date: 19/02/2017
- * Time: 22:06
+ * Class XMLFile
+ * XML file wrappes.
+ * Enables to get XMLWriter of given file.
  */
 class XMLFile extends File
 {
 
-
+    /**
+     * Method to retrieve XMLWriter API object.
+     * @return XMLWriter object to communicate with libxml API
+     * @{see XMLWriter}
+     */
     function getXmlWriter()
     {
         $xml_writer = new XMLWriter();

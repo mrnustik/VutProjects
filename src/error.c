b73d9cc0531f0f34dd8cf4d3234b4e64a3a1691e
Michal Mrnustik <michal.mrnustik@outlook.com>
2017-02-26 21:26:30 +0100

    Add error.c/error.h

---
+++ src/error.c
@@ -0,0 +1,5 @@
+//
+// Created by Michal Mrnustik on 26/02/2017.
+//
+
+#include "error.h"
---
+++ src/error.h
@@ -0,0 +1,9 @@
+//
+// Created by Michal Mrnustik on 26/02/2017.
+//
+
+#ifndef ERROR_H
+#define ERROR_H
+
+
+#endif //ERROR_H

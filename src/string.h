f7d19116233340bc372cb78061db4ef01c74e93f
Michal Mrnustik <michal.mrnustik@outlook.com>
2017-03-02 15:30:27 +0100

    Add String type

--- src/string.h
+++ src/string.h
@@ -7,6 +7,18 @@
 
 #include <string.h>
 
+#define STRING_INIT_SIZE 256
+
+typedef struct tString{
+    char* pointer;
+    int allocated;
+    int used;
+} tString;
+
+tString* stringInit();
+void stringAppend(tString* string, char* data);
+void stringErase(tString* string);
+int stringFind(tString* string, const char* find);
 int substringPosition(char* string, const char* substring);
 
 #endif //FTREST_STRING_H

dcddad2ec50a9ee2eeb62859d7261dedfc3920d8
Michal Mrnustik <michal.mrnustik@outlook.com>
2017-03-11 19:57:47 +0100

    Initial commit

---
+++ Library/Memory.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Michal Mrnustik on 11/03/2017.
+//
+
+#include "Memory.h"
---
+++ Library/Operation.h
@@ -0,0 +1,8 @@
+//
+// Created by Michal Mrnustik on 11/03/2017.
+//
+
+#ifndef IPK_PROJ1_CPP_OPERATION_H
+#define IPK_PROJ1_CPP_OPERATION_H
+
+#endif //IPK_PROJ1_CPP_OPERATION_H

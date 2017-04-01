02503703af966eb30b81b9122dc14e17acd4831d
Mrnda <michal.mrnustik@outlook.com>
2017-04-01 17:58:15 +0200

    Add project files

---
+++ SolitaireProject/src/net/mrnustik/university/solitaire/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.mrnustik.university.solitaire;
+
+/**
+ *
+ * @author micha
+ */
+public class Main {
+    public static void Main(String args[])
+    {
+        
+    }
+}

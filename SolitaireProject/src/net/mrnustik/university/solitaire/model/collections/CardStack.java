afd619407fb51c2eb73c13ee48f6b53d9569df9c
Mrnda <michal.mrnustik@outlook.com>
2017-04-01 18:19:49 +0200

    Add CardStack interface
    Interface used for objects that can take/give more Cards

---
+++ SolitaireProject/src/net/mrnustik/university/solitaire/model/collections/CardStack.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.mrnustik.university.solitaire.model.collections;
+
+/**
+ *
+ * @author micha
+ */
+public interface CardStack {
+    
+}

977452b7741d9ad21a15172420f59be185b287ca
Mrnda <michal.mrnustik@outlook.com>
2017-04-01 18:30:42 +0200

    Add TargetStacker
    TargetStacker is an implementation of a Stacker that can only have cards of specific colour inside.

---
+++ SolitaireProject/src/net/mrnustik/university/solitaire/model/collections/implementation/TargetStacker.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.mrnustik.university.solitaire.model.collections.implementation;
+
+import net.mrnustik.university.solitaire.model.Card;
+
+/**
+ *
+ * @author micha
+ */
+public class TargetStacker extends SolitaireStacker{
+
+    private Card.Color mColor;
+
+    public TargetStacker(Card.Color mColor) {
+        this.mColor = mColor;
+    }
+    
+    @Override
+    public boolean put(Card card) {
+        if(card.getColor() == this.mColor) {
+           if(this.isEmpty()){
+               if(card.getValue() == 1)
+                    return super.put(card);
+           }
+           else if(this.get().compareTo(card) == 1)
+           {
+               return super.put(card);
+           }
+        }
+        return false;
+    }
+    
+}

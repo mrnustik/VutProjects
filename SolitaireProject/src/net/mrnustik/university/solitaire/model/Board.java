c631e46b4735a7f89a01acd2f59047b931b54ad6
Mrnda <michal.mrnustik@outlook.com>
2017-04-01 19:51:35 +0200

    Add Board class
    Board implementation to combine all pieces together

---
+++ SolitaireProject/src/net/mrnustik/university/solitaire/model/Board.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.mrnustik.university.solitaire.model;
+
+import net.mrnustik.university.solitaire.board.base.AbstractFactory;
+import net.mrnustik.university.solitaire.model.collections.CardDeck;
+import net.mrnustik.university.solitaire.model.collections.CardStack;
+import net.mrnustik.university.solitaire.model.collections.CardStacker;
+
+/**
+ *
+ * @author micha
+ */
+public class Board {
+    private final CardDeck mDeck;
+    private final CardStacker mStacker;
+    private final CardStacker[] mTargets;
+    private final CardStack[] mWorkingStacks;
+
+    private static final int WORKING_COUNT = 7;
+    
+    public Board(AbstractFactory factory) {
+        this.mDeck = factory.createCardDeck();
+        this.mStacker = factory.createPutDownStacker();
+        this.mTargets = new CardStacker[4];
+        for(int i = 0; i < this.mTargets.length; i++) {
+            this.mTargets[i] = factory.createTargetStacker(Card.Color.values()[i]);
+        }
+        this.mWorkingStacks = new CardStack[WORKING_COUNT];
+        for(int i = 0; i< this.mWorkingStacks.length; i++){
+            this.mWorkingStacks[i] = factory.createWorkingPack(i+1, this.mDeck);
+        }
+        
+    }
+}

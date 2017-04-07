34a9c320e5f5c68ec8d81531ace25e103853f747
Michal Mrnustik <michal.mrnustik@outlook.com>
2017-04-07 14:12:24 +0200

    Add game cards

---
+++ SolitaireProject/src/net/mrnustik/university/solitaire/gui/view/CardView.java
@@ -0,0 +1,22 @@
+package net.mrnustik.university.solitaire.gui.view;
+
+import net.mrnustik.university.solitaire.model.Card;
+
+import javax.swing.*;
+
+/**
+ * Created by mrnda on 07/04/2017.
+ */
+public class CardView extends JButton {
+
+    private Card mCard;
+
+    public CardView(Card card) {
+        this.mCard = card;
+    }
+
+
+
+
+
+}

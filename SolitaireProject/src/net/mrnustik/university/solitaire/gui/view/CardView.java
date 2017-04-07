32e05627da34575bede69e1d2507afc25eceb053
Michal Mrnustik <michal.mrnustik@outlook.com>
2017-04-07 14:23:12 +0200

    Add imageloading into  CardView

--- SolitaireProject/src/net/mrnustik/university/solitaire/gui/view/CardView.java
+++ SolitaireProject/src/net/mrnustik/university/solitaire/gui/view/CardView.java
@@ -3,6 +3,7 @@ package net.mrnustik.university.solitaire.gui.view;
 import net.mrnustik.university.solitaire.model.Card;
 
 import javax.swing.*;
+import java.net.URL;
 
 /**
  * Created by mrnda on 07/04/2017.
@@ -15,8 +16,18 @@ public class CardView extends JButton {
         this.mCard = card;
     }
 
+    @Override
+    public Icon getIcon() {
+        URL resource = null;
 
-
-
-
+        if(mCard.isFaceUp()) {
+            resource = getClass().getResource("/net/mrnustik/university/solitaire/gui/images/" + mCard.toString() + ".png");
+        } else {
+            resource = getClass().getResource("/net/mrnustik/university/solitaire/gui/images/card_default.png");
+        }
+        if(resource != null)
+            return new ImageIcon(resource);
+        else
+            return super.getIcon();
+    }
 }

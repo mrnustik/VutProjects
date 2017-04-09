ac9cf6d36ffbc9400126b4ac9c495c3e06ee28c9
Mrnda <michal.mrnustik@outlook.com>
2017-04-09 14:39:40 +0200

    Add game menu to GamePanel

--- SolitaireProject/src/net/mrnustik/university/solitaire/gui/panels/GamePanel.java
+++ SolitaireProject/src/net/mrnustik/university/solitaire/gui/panels/GamePanel.java
@@ -2,13 +2,15 @@ package net.mrnustik.university.solitaire.gui.panels;
 
 import net.mrnustik.university.solitaire.factory.SolitaireFactory;
 import net.mrnustik.university.solitaire.gui.view.CardView;
+import net.mrnustik.university.solitaire.io.BoardSaver;
+import net.mrnustik.university.solitaire.io.json.JsonBoardSaver;
 import net.mrnustik.university.solitaire.model.Board;
 import net.mrnustik.university.solitaire.model.Card;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.io.File;
 
 /**
  * Created by mrnda on 4/7/2017.
@@ -21,6 +23,8 @@ public class GamePanel extends JPanel {
     private CardView[] targets;
 
     private Card selected = null;
+    private JLabel scoreLabel;
+
 
     public GamePanel() {
         super(new GridLayout(2,6, 10 ,10));
@@ -33,23 +37,21 @@ public class GamePanel extends JPanel {
     }
 
     private void initViews() {
-        deck = new CardView(board.getDeckTop());
-        add(deck);
-        stacker = new CardView(board.getStackTop());
-        add(stacker);
-        deck.addActionListener(l->{
-            board.flipFromDeck();
-            paintBoard();
-        });
+        setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+        initDeck();
+        initMenu();
+        initTargetPacks();
+        initWorkingStacks();
+    }
 
-        stacker.addActionListener(l->{
-            selected = stacker.getCard();
-        });
+    private void initWorkingStacks() {
+        for(int i = 0; i < 7; i++)
+        {
+            add(new CardView(board.getStackTop()));
+        }
+    }
 
-        JPanel separator = new JPanel();
-        separator.setBackground(Color.decode("#146a2c"));
-        add(separator);
-
+    private void initTargetPacks() {
         targets = new CardView[4];
         for(int i = 0; i < 4; i++)
         {
@@ -65,12 +67,70 @@ public class GamePanel extends JPanel {
 
             add(targets[i]);
         }
+    }
 
+    private void initDeck() {
+        deck = new CardView(board.getDeckTop());
+        add(deck);
+        stacker = new CardView(board.getStackTop());
+        add(stacker);
+        deck.addActionListener(l->{
+            board.flipFromDeck();
+            paintBoard();
+        });
 
+        stacker.addActionListener(l->{
+            selected = stacker.getCard();
+        });
+    }
 
-        for(int i = 0; i < 7; i++)
-        {
-            add(new CardView(board.getStackTop()));
+    private void initMenu() {
+        JPanel gameMenu = new JPanel();
+        gameMenu.setBackground(Color.decode("#146a2c"));
+        gameMenu.setLayout(new BoxLayout(gameMenu, BoxLayout.PAGE_AXIS));
+        add(gameMenu);
+
+        gameMenu.add(Box.createVerticalGlue());
+
+        JButton saveBtn = new JButton("Save game");
+        saveBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
+        saveBtn.addActionListener(l->{
+            JFileChooser fileChooser = new JFileChooser();
+            FileNameExtensionFilter xmlFilter = new FileNameExtensionFilter("JSON files (*.json)", "json");
+            fileChooser.addChoosableFileFilter(xmlFilter);
+            fileChooser.setFileFilter(xmlFilter);
+            int result = fileChooser.showSaveDialog(this);
+            if(result == JFileChooser.APPROVE_OPTION) {
+                File file = fileChooser.getSelectedFile();
+                saveGame(file.getPath());
+            }
+        });
+        gameMenu.add(saveBtn);
+        gameMenu.add(Box.createVerticalStrut(10));
+        JButton undoBtn = new JButton("Undo");
+        undoBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
+        undoBtn.addActionListener(l->{
+            board.undo();
+            paintBoard();
+        });
+        gameMenu.add(undoBtn);
+        gameMenu.add(Box.createVerticalStrut(10));
+        JButton endBtn = new JButton("End game");
+        endBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
+        endBtn.addActionListener(l->{
+            JFrame topFrame = (JFrame) SwingUtilities.getWindowAncestor(this);
+            topFrame.remove(this);
+        });
+        gameMenu.add(endBtn);
+        gameMenu.add(Box.createVerticalGlue());
+    }
+
+    private void saveGame(String path) {
+        BoardSaver saver = new JsonBoardSaver();
+        try {
+            saver.save(path, board);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(this, "Something wrong happened when saving." + e.getMessage());
         }
     }
 

0111ca2a4fae75dc063b4805ec159cc22cab7faa
Michal Mrnuštík <michal.mrnustik@outlook.com>
2017-11-11 23:55:40 +0100

    Finalize documentation

--- src/ArpScanner.cpp
+++ src/ArpScanner.cpp
@@ -21,24 +21,23 @@ ArpScanner::~ArpScanner()
 
 
 typedef struct {
-	struct {	                //  OFFSET		Bajt
-		uint8_t dst_mac[6];		//  0 -  5		+6
-		uint8_t src_mac[6];		//  6 - 11		+6
-		uint8_t type[2];		// 12 - 13		+2
+	struct {	                
+		uint8_t destinationMac[6];
+		uint8_t sourceMac[6];
+		uint8_t type[2];
 	} eth;
-	struct {					//  OFFSET		Bajt
-		uint16_t htype;         // 14 - 15		+2
-		uint16_t ptype;			// 16 - 17		+2
-		uint8_t hlen;			// 18 - 18		+1
-		uint8_t plen;			// 19 - 19		+1
-		uint16_t opcode;		// 20 - 21		+2
-		uint8_t sender_mac[6];	// 22 - 27		+6
-		uint8_t sender_ip[4];	// 28 - 31		+4
-		uint8_t target_mac[6];	// 32 - 37		+6
-		uint8_t target_ip[4];	// 38 - 41		+4
+	struct {
+		uint16_t hardwareType;
+		uint16_t protocolType;
+		uint8_t hardwareLength;
+		uint8_t protocolLength;
+		uint16_t operationCode;
+		uint8_t senderMac[6];
+		uint8_t senderIp[4];
+		uint8_t targetMac[6];
+		uint8_t targetIp[4];
 	} arp;
-
-	uint8_t padding[36];          // 44 - XX
+	uint8_t padding[36];
 } arpEthPacket;
 
 std::vector<IpAddress> ArpScanner::ScanNetwork(IpNetwork network, std::string adapter) {
@@ -58,28 +57,28 @@ std::vector<IpAddress> ArpScanner::ScanNetwork(IpNetwork network, std::string ad
     arpEthPacket packet;
     bzero(&packet, sizeof(packet));
     uint8_t dstAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
-    memcpy(packet.eth.dst_mac, dstAddress , sizeof(dstAddress));
-    memcpy(packet.eth.src_mac, macAddress, sizeof(macAddress));
+    memcpy(packet.eth.destinationMac, dstAddress , sizeof(dstAddress));
+    memcpy(packet.eth.sourceMac, macAddress, sizeof(macAddress));
     packet.eth.type[0] = ETH_P_ARP / 256;
     packet.eth.type[1] = ETH_P_ARP % 256;
 
-    packet.arp.htype  = htons( 1 );             // 1 == Ethernet
-    packet.arp.ptype  = htons(ETH_P_IP);
-    packet.arp.hlen   = sizeof(uint8_t) * 6;
-    packet.arp.plen   = sizeof(uint8_t) * 4;
-    packet.arp.opcode = htons( ARPOP_REQUEST );
+    packet.arp.hardwareType  = htons( 1 );             // 1 == Ethernet
+    packet.arp.protocolType  = htons(ETH_P_IP);
+    packet.arp.hardwareLength   = sizeof(uint8_t) * 6;
+    packet.arp.protocolLength   = sizeof(uint8_t) * 4;
+    packet.arp.operationCode = htons( ARPOP_REQUEST );
 
-    memcpy(packet.arp.sender_mac, macAddress, sizeof(macAddress));
-    memcpy(packet.arp.target_mac, dstAddress, sizeof(dstAddress));
+    memcpy(packet.arp.senderMac, macAddress, sizeof(macAddress));
+    memcpy(packet.arp.targetMac, dstAddress, sizeof(dstAddress));
     auto srcIp = deviceIp.ToInAddr().s_addr;
-    memcpy(packet.arp.sender_ip, &srcIp , sizeof(uint8_t) * 4);
+    memcpy(packet.arp.senderIp, &srcIp , sizeof(uint8_t) * 4);
 
     auto enumerator = network.GetEnumerator();
     while(enumerator.MoveNext())
     {
         auto currentAddress = enumerator.Current();
         auto dstIp = currentAddress.ToInAddr().s_addr;
-        memcpy(packet.arp.target_ip, &dstIp, sizeof(dstIp));
+        memcpy(packet.arp.targetIp, &dstIp, sizeof(dstIp));
         if (sendto(socket, &packet, sizeof(arpEthPacket), 0, (struct sockaddr *)&device, sizeof(struct sockaddr_ll)) <= 0)
         {
             Logger::Error("Arp Scanner", "Sending", errno);
@@ -106,10 +105,10 @@ std::vector<IpAddress> ArpScanner::ScanNetwork(IpNetwork network, std::string ad
         int bufferSize = read(socket, buffer, sizeof(buffer));
         auto *arpHeader = (arpEthPacket *) (buffer);
 
-        if (ntohs(arpHeader->arp.opcode) == ARPOP_REPLY) {
+        if (ntohs(arpHeader->arp.operationCode) == ARPOP_REPLY) {
             Logger::Debug("ARP Scan", "Reply");
             unsigned int intAddr;
-            memcpy(&intAddr, arpHeader->arp.sender_ip, 4);
+            memcpy(&intAddr, arpHeader->arp.senderIp, 4);
             IpAddress addr(ntohl(intAddr));
             addressVector.push_back(addr);
         }

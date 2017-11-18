b234144ac8bb42294db4c25fa9ad1e2a9117a45a
Michal Mrnuštík <michal.mrnustik@outlook.com>
2017-11-18 15:10:18 +0100

    Add header to all pages

---
+++ src/InformationSystem/InformationSystem.Web/ViewModels/User/Repairs/OrderViewModel.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DotVVM.Framework.Runtime.Filters;
+using DotVVM.Framework.ViewModel;
+
+namespace InformationSystem.Web.ViewModels.User.Repairs
+{
+    [Authorize]
+    public class OrderViewModel : MasterPageViewModel
+    {
+            
+    }
+}
+

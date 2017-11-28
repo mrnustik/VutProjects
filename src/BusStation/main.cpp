bf821be8dedc59596f38d6b8bd3571287dba1bde
Mrnda <michal.mrnustik@outlook.com>
2017-11-28 14:12:45 +0100

    Add main file

---
+++ src/BusStation/main.cpp
@@ -0,0 +1,174 @@
+﻿
+#include <iostream>
+#include <simlib.h>
+
+#define DEBUG true
+
+#define START_OF_SIMULATION 60 * 8 // 8:00
+#define END_OF_SIMULATION	60 * 24 //one day
+#define SIMULATION_LENGTH END_OF_SIMULATION - START_OF_SIMULATION
+
+#define NUMBER_OF_PLATFORMS 6
+
+#define AVERAGE_DELAY_PRAHA				10
+#define PROBABILITY_DELAY_PRAHA			0.2
+#define ARRIVAL_INTERVAL_PRAHA			15
+
+#define AVERAGE_DELAY_KROMERIZ			5
+#define PROBABILITY_DELAY_KROMERIZ		0.1
+#define ARRIVAL_INTERVAL_KROMERIZ		25
+
+#define AVERAGE_DELAY_BRATISLAVA		5
+#define PROBABILITY_DELAY_BRATISLAVA	0.1
+#define ARRIVAL_INTERVAL_BRATISLAVA		30
+
+#define AVERAGE_DELAY_VIDEN				5
+#define PROBABILITY_DELAY_VIDEN			0.1
+#define ARRIVAL_INTERVAL_VIDEN			40
+
+#define LOCAL_BUS_INTERVAL				5
+
+#define PLATFORM_WAIT_TIME				15
+
+Facility Platforms[NUMBER_OF_PLATFORMS];
+Queue PlatformsQueue;
+
+Histogram DelayHistogram("Delay", 0, SIMULATION_LENGTH, 1);
+Histogram LocalDelayHistogram("Local delay", 0, SIMULATION_LENGTH, 1);
+
+class Logger 
+{
+public:
+	static void DebugLog(std::string category, std::string message)
+	{
+		if (DEBUG)
+		{
+			int time = (int)Time;
+			int timeHours = time / 60;
+			int timeMinutes = time % 60;
+			std::cout << std::to_string(timeHours) << ":" << std::to_string(timeMinutes) << " [" << category << "]: " << message << std::endl;
+		}
+	}
+};
+
+class Bus : public Process
+{
+	double delayValue = 0;
+	double delayPercentage = 0;
+	double delay = 0;
+public:
+	Bus(double delayPercentage, double delayValue) {
+		this->delayPercentage = delayPercentage;
+		this->delayValue = delayValue;
+	}
+
+	void Behavior()
+	{
+		if (Random() <= delayPercentage) 
+		{
+			double delayTime = Exponential(this->delayValue);
+			Logger::DebugLog("Bus", "Delayed bus ariving " + std::to_string(delayTime));
+			this->Wait(delayTime);
+			delay += delayTime;
+		}
+		double startWaitingTime = -1;
+FindPlatform:
+		int platformNumber = -1;
+		for (int i = 0; i < NUMBER_OF_PLATFORMS; i++) {
+			if (Platforms[i].Busy() == false) 
+			{
+				if (startWaitingTime > 0) 
+				{
+					auto localDelay = Time - startWaitingTime;
+					LocalDelayHistogram(localDelay);
+					delay += localDelay;
+				}
+				Seize(Platforms[i]);
+				platformNumber = i;
+				break;
+			}
+		}
+
+		if (platformNumber == -1) 
+		{
+			Into(PlatformsQueue);
+			startWaitingTime = Time;
+			Passivate();
+			goto FindPlatform;
+		}
+
+		Wait(PLATFORM_WAIT_TIME);
+
+		Release(Platforms[platformNumber]);
+		if (PlatformsQueue.Empty() == false) 
+		{
+			auto bus = PlatformsQueue.GetFirst();
+			bus->Activate();
+		}
+
+		DelayHistogram(delay);
+	}
+
+};
+
+class RemoteBusGenerator : public Event {
+private:
+	double delayValue = 0;
+	double probabilityOfDelay = 0;
+	std::string direction = "";
+	double arrivalInterval = 0;
+public:
+	RemoteBusGenerator(std::string direction, double delayValue, double probabilityOfDelay, double arivalInterval) : Event()
+	{
+		this->direction = direction;
+		this->delayValue = delayValue;
+		this->probabilityOfDelay = probabilityOfDelay;
+		this->arrivalInterval = arivalInterval;
+	}
+
+	void Behavior()
+	{
+		auto bus = new Bus(probabilityOfDelay, delayValue);
+		Logger::DebugLog("Bus generator", "Bus arriving from " + direction);
+		bus->Activate();
+		this->Activate(Time + arrivalInterval);
+	}
+};
+
+
+class LocalBusGenerator : public Event{
+private:
+	double arrivalInterval = 0;
+public:
+	LocalBusGenerator(double arrivalInterval)
+	{
+		this->arrivalInterval = arrivalInterval;
+	}
+
+	void Behavior() 
+	{
+		auto bus = new Bus(0, 0);
+		Logger::DebugLog("Local bus generator", "Local bus arrived");
+		bus->Activate();
+		this->Activate(Time + arrivalInterval);
+	}
+};
+
+
+int main() {
+	Print("Brno Bus Station simulace\n");
+	Init(START_OF_SIMULATION, END_OF_SIMULATION);
+	(new RemoteBusGenerator("Praha", AVERAGE_DELAY_PRAHA, PROBABILITY_DELAY_PRAHA, ARRIVAL_INTERVAL_PRAHA))->Activate();
+	(new RemoteBusGenerator("Kroměříž", AVERAGE_DELAY_KROMERIZ, PROBABILITY_DELAY_KROMERIZ, ARRIVAL_INTERVAL_KROMERIZ))->Activate();
+	(new RemoteBusGenerator("Bratislav", AVERAGE_DELAY_BRATISLAVA, PROBABILITY_DELAY_BRATISLAVA, ARRIVAL_INTERVAL_BRATISLAVA))->Activate();
+	(new RemoteBusGenerator("Vídeň", AVERAGE_DELAY_VIDEN, PROBABILITY_DELAY_VIDEN, ARRIVAL_INTERVAL_VIDEN))->Activate();
+	(new LocalBusGenerator(LOCAL_BUS_INTERVAL))->Activate();
+	Run();
+	for (int i = 0; i < NUMBER_OF_PLATFORMS; i++) 
+	{
+		Logger::DebugLog("Statistics", "Platform number " + std::to_string((i + 1)));
+		Platforms[i].Output(); 
+	}
+	DelayHistogram.Output();
+	LocalDelayHistogram.Output();
+}

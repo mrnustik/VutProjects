37457894e3dcc2357a82c1b6cbc1c1249acc914a
Michal Mrnuštík <michal.mrnustik@outlook.com>
2017-12-07 17:41:32 +0000

    Update tests in order they work

--- src/SvnClient/SvnClient.Tests/SvnRepositoryTests.cs
+++ src/SvnClient/SvnClient.Tests/SvnRepositoryTests.cs
@@ -11,8 +11,10 @@ namespace SvnClient.Tests
         [TestMethod]
         public void SvnRepository_GetHistory_ReturnsData()
         {
+
+            var connection = SvnConnection.OpenConnection("Connection", "https://svn.code.sf.net/p/passgenwin/code/");
             var repository = new SvnRepository();
-            var svnCommitModels = repository.GetHistory("https://svn.code.sf.net/p/passgenwin/code/");
+            var svnCommitModels = repository.GetHistory(connection);
             Assert.IsTrue(svnCommitModels.Take(10).Count() > 0);
             Assert.IsTrue(svnCommitModels.Skip(10).Take(10).Count() > 0);
         }
